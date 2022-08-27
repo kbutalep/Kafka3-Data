91ed7c51d77fc7ac86ca10ef4ecf5146a6f3de2f
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-08-27 15:14:24 -0400

    phase1 db connection persistence working

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,14 +1,33 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
-import mysql.connector
-import config
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+#import config
 
-mydb = mysql.connector.connect(
-    host= "localhost",
-    user= config.user,
-    password = config.password
-)
 
+
+user = 'kendra'
+pwd = 'kendra123'
+host = 'Zipcoders-MacBook-Pro-45.local'
+dbname = 'zipbank'
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=host, db=dbname, user=user, pw=pwd))
+Base = declarative_base()
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    custid = Column(Integer)
+    type = Column(String(255), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
+
+    def __init__(self, custid, type, date, amt):
+        self.custid = custid
+        self.type = type
+        self.date = date
+        self.amt = amt
 
 class XactionConsumer:
     def __init__(self):
@@ -25,7 +44,10 @@ class XactionConsumer:
         # THE PROBLEM is every time we re-run the Consumer, ALL our customer
         # data gets lost!
         # add a way to connect to your database here.
+        self.engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=host, db=dbname, user=user, pw=pwd))
 
+        self.Session = sessionmaker
+        self.session = self.Session(self.engine)
         #Go back to the readme.
 
     def handleMessages(self):
@@ -41,6 +63,11 @@ class XactionConsumer:
             else:
                 self.custBalances[message['custid']] -= message['amt']
             print(self.custBalances)
+            xaction = Transaction(message['custid'], message['type'], message['date'], message['amt'])
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            session.add(xaction)
+            session.commit()
 
 if __name__ == "__main__":
     c = XactionConsumer()
